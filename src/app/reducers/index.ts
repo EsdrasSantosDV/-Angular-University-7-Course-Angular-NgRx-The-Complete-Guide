f11232bd77cb3727f60f9cccf1d019e995c121ff
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-01-31 09:47:34 -0300

    feat:meta reducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,4 @@
-import {Action, ActionReducerMap, MetaReducer} from "@ngrx/store";
+import { Action, ActionReducer, ActionReducerMap, MetaReducer } from "@ngrx/store";
 import {environment} from "../../environments/environment";
 import { routerReducer } from "@ngrx/router-store";
 
@@ -13,4 +13,21 @@ export const reducers:ActionReducerMap<AppState>={
   router:routerReducer
 };
 
-export const metaReducers:MetaReducer<AppState>[]=!environment.production ?[]:[];
+
+//META REDUCER E BASICMANETE UM REDUCER QUE ACONTECE ANTES DOS REDCUERS 'PADROES'
+//Aqui,neessa reducer executamos aqui nossa funcionalidade e pegamos o estado e
+// a ação que acabou de ser despachada e passamos isso para o próximo produtor em nossa cadeia de reducers
+export function logger(reducer:ActionReducer<any>)
+  : ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("action", action);
+
+    return reducer(state, action);
+  }
+
+}
+
+export const metaReducers: MetaReducer<AppState>[] =
+  !environment.production ? [logger] : [];
+//AQUI E A ORDEM DOS REDUCERS QUE VOCE QUER , PODE TER VARISO META REDUCERS
